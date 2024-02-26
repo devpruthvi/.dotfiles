276c473fc63ef1db4b0b80f0be19cf4639efbf60
Pruthvi Raj N V <pruthvi.n.v@gmail.com>
2024-02-26 19:57:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Pre-requisites
+- Arch
+    - Install Paru from AUR
+    - Run `paru -S fzf ripgrep bat git-delta wezterm alacritty`
+- MacOS
+    - Install Brew
+    - Run `brew install fzf ripgrep bat git-delta`
+    - Run `brew install --cask wezterm alacritty`
---
+++ dot_dotfiles/oh-my-zsh/init.zsh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+# disable updates (refreshPeriod set in .chezmoiexternal)
+zstyle ':omz:update' mode disabled
+
+# powerlevel10k
+ZSH_THEME="powerlevel10k/powerlevel10k"
+source $DOTFILES/.p10k.zsh
+
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=100
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets root )
+
+# Default Plugins
+plugins=(
+	brew
+	extract
+	fzf
+	git
+	macos
+	ripgrep
+	z
+	fzf
+)
+
+# Custom Plugins (installed with .chezmoiexternal)
+plugins+=(
+	ports
+	mkc
+	zsh-completions
+	fast-syntax-highlighting
+	zsh-history-substring-search
+	zsh-autosuggestions
+)
