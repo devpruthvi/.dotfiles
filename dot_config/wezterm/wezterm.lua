8134d9580a7c3636f015600ddeaf5f0cd96e1ca5
Pruthvi Raj N V <pruthvi.n.v@gmail.com>
2024-02-26 20:03:07 +0530

    Add sample wezterm configuration

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.window_background_opacity = 0.93
+
+config.font = wezterm.font 'JetBrainsMono NerdFont'
+
+return config
