29bfed618321f5160ea9fa4035d67da48ad49751
Pruthvi Raj N V <pruthvi.n.v@gmail.com>
2024-03-12 18:37:47 +0530

    Add configuration for dual monitor setup

---
+++ dot_screenlayout/private_executable_dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --output DP-3 --off --output HDMI-0 --off --output HDMI-1 --off --output HDMI-2 --off --output DP-4 --mode 2560x1440 --pos 1440x560 --rotate normal --output DP-5 --off --output None-1-1 --off
