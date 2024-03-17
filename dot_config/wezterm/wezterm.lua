cad4baca68c0653ce1e6e8a757f09d71d17c366b
Pruthvi Raj N V <pruthvi.n.v@gmail.com>
2024-03-17 06:47:03 +0530

    Update wezterm font

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.window_background_opacity = 0.93
 
-config.font = wezterm.font("JetBrainsMono NerdFont")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 
 config.color_scheme = "rose-pine"
 
