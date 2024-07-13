db498c2d04ab3a0fe9a0e47d56eeac37e9550eef
Pruthvi Raj N V <pruthvi.n.v@gmail.com>
2024-07-13 23:52:04 +0530

    Fix typo in conform

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
       {
         "<leader>cf",
         function()
-          require("confirm").format({ async = true, lsp_fallback = true })
+          require("conform").format({ async = true, lsp_fallback = true })
         end,
         mode = "",
         desc = "[C]ode [F]ormat",
